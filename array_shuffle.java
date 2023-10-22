3a68903fdff4c17faa79ab458920d3da5f0f0611
Shahnawaz Akhtar <58129001+Shahnawaz23@users.noreply.github.com>
2023-10-22 15:34:56 +0530

    Create array_shuffle.java

---
+++ array_shuffle.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ShuffleArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void shuffleArray(int[] arr) {
+        int n = arr.length;
+        Random random = new Random();
+
+        for (int i = n - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+
+            // Swap arr[i] and arr[j]
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
